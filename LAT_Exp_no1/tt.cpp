f0750ee90bea3e430b4c5c81b62f5162bd104dad
A.MINH Ng <nam140408@gmail.com>
2024-06-02 22:54:09 +0700

    ok

---
+++ LAT_Exp_no1/tt.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+signed main(){
+    //freopen("Minimax.INP","r",stdin);
+    freopen("Minimax.OUT","w",stdout);
+    for (int i = 1; i <= 3*3*3; i++)
+    {
+        cout << 3 << " " << i << " ";
+    }
+    
+}
